0e8ed3936e8a302adfdc5a6afe6577daf88a03ce
zhuangpeifen <zhuangpeifen@rrjc.com>
2018-03-28 21:33:29 +0800

    no message

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+      <Header/>
+      <section class="tab">
+        <div class="tab-item">商品</div>
+        <div class="tab-item">评价</div>
+        <div class="tab-item">商家</div>
+      </section>
+      <section class="content">
+        内容
+      </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import Header from './components/header/header';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang='scss' scoped>
+.tab {
+  display: flex;
+  width: 100%;
+  height: 40px;
+  line-height: 40px;
+  .tab-item {
+    flex: 1;
+    text-align: center;
+  }
 }
 </style>
---
+++ src/components/header/header.vue
@@ -0,0 +1,22 @@
+<!-- header -->
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'i am header'
+    };
+  },
+
+  components: {},
+
+  methods: {}
+};
+</script>
+
+<style lang='scss' scoped>
+
+</style>
